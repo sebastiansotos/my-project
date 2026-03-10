e6280d493e65d02064caf704c32c3260edac3987
sebastiansotos <sebastiansotogamerxd@gmail.com>
2026-03-10 15:57:41 -0400

    Modified files:     src/App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sebas Web
+          Sebas Web tu sabe
         </p>
        <Primercomponente /> 
        <hr/>
